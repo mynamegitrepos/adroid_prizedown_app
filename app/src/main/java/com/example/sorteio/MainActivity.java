e618bcb136de25b121e8791c5a439cd2c32300d5
mynamegitrepos <samuelpereiradesouza665@gmail.com>
2022-05-10 18:43:06 -0300

    initial commit

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNumero(View view){
+
+        TextView text = findViewById(R.id.textResultado);
+        int number = new Random().nextInt(11); //0 até 10
+
+        text.setText("Número: " + number );
+
+    }
+}
